b2cdafdb891f223ad380d55249289d195d6c908c
Dan0011037 <dan00.11037@gmail.com>
2025-12-18 09:14:42 +0100

    Update casilla.java

--- src/casilla.java
+++ src/casilla.java
@@ -8,10 +8,12 @@ public class casilla {
         this.columna = columna;
     }
 
+    public int getColumna() {return columna;}
+    public int getFila() {return fila;}
+
     public pieza getPieza() {
         return pieza;
     }
-
     public void setPieza(pieza pieza) {
         this.pieza = pieza;
     }
