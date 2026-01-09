d0617129debf5c517d3ce26c91f87293883173a1
Rauli72 <raulcayuelamacia@gmail.com>
2026-01-09 08:40:36 +0100

    Delete src/MovimientoTorre.java

--- src/MovimientoTorre.java
+++
@@ -1,8 +0,0 @@
-public class MovimientoTorre {
-    public static boolean esValido(int filaInicio, int columnaInicio, int filaFin, int columnaFin) {
-        int filaDiferencia = Math.abs(filaFin - filaInicio);
-        int columnaDiferencia = Math.abs(columnaFin - columnaInicio);
-        // Movimiento en línea recta
-        return filaDiferencia == 0 || columnaDiferencia == 0;
-    }
-}
