5da1dcfe742aed2b464384a3310e95e0475585ce
Rauli72 <raulcayuelamacia@gmail.com>
2026-01-09 08:40:00 +0100

    Delete src/MovimientoCaballo.java

--- src/MovimientoCaballo.java
+++
@@ -1,8 +0,0 @@
-public class MovimientoCaballo {
-    public static boolean esValido(int filaInicio, int columnaInicio, int filaFin, int columnaFin) {
-        int filaDiferencia = Math.abs(filaFin - filaInicio);
-        int columnaDiferencia = Math.abs(columnaFin - columnaInicio);
-        // Movimiento en L
-        return (filaDiferencia == 2 && columnaDiferencia == 1) || (filaDiferencia == 1 && columnaDiferencia == 2);
-    }
-}
